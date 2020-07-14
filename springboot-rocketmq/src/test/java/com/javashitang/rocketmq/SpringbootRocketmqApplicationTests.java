4f7f2bc3299c3f8c2a7ff733f57e702901e7dbe4
lilimin <290059742@qq.com>
2020-07-14 20:03:38 +0800

    修改启动类名

--- springboot-rocketmq/src/test/java/com/javashitang/rocketmq/SpringbootRocketmqApplicationTests.java
+++ springboot-rocketmq/src/test/java/com/javashitang/rocketmq/SpringbootRocketmqApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootRabbitmqApplicationTests {
+public class SpringbootRocketmqApplicationTests {
 
     @Test
     void contextLoads() {
